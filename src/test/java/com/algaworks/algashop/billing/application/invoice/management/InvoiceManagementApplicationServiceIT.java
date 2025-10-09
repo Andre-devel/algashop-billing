8667d22757ec738488800687f97979bcc5c0a815
Andre goncalves <andrevilanovagon@hotmail.com>
2025-10-08 21:14:36 -0300

    feat: add integration test for payment processing in InvoiceManagementApplicationService

--- src/test/java/com/algaworks/algashop/billing/application/invoice/management/InvoiceManagementApplicationServiceIT.java
+++ src/test/java/com/algaworks/algashop/billing/application/invoice/management/InvoiceManagementApplicationServiceIT.java
@@ -6,13 +6,19 @@ import com.algaworks.algashop.billing.domain.model.creditcard.CreditCardTestData
 import com.algaworks.algashop.billing.domain.model.invoice.Invoice;
 import com.algaworks.algashop.billing.domain.model.invoice.InvoiceRepository;
 import com.algaworks.algashop.billing.domain.model.invoice.InvoiceStatus;
+import com.algaworks.algashop.billing.domain.model.invoice.InvoiceTestDataBuilder;
 import com.algaworks.algashop.billing.domain.model.invoice.InvoicingService;
 import com.algaworks.algashop.billing.domain.model.invoice.PaymentMethod;
+import com.algaworks.algashop.billing.domain.model.invoice.payament.Payment;
+import com.algaworks.algashop.billing.domain.model.invoice.payament.PaymentGatewayService;
+import com.algaworks.algashop.billing.domain.model.invoice.payament.PaymentRequest;
+import com.algaworks.algashop.billing.domain.model.invoice.payament.PaymentStatus;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.bean.override.mockito.MockitoSpyBean;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,9 +36,12 @@ class InvoiceManagementApplicationServiceIT {
     
     @Autowired
     private CreditCardRepository creditCardRepository;
-    
+
     @MockitoSpyBean
     private InvoicingService invoicingService;
+
+    @MockitoBean
+    private PaymentGatewayService paymentGatewayService;
     
     @Test
     public void shouldGenerateInvoiceWithCreditCardAsPayment() {
@@ -83,5 +92,30 @@ class InvoiceManagementApplicationServiceIT {
 
 
         Mockito.verify(invoicingService).issue(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any());
+    }
+    
+    @Test
+    public void shouldProcessPayment() {
+        Invoice invoice = InvoiceTestDataBuilder.anInvoice().build();
+        invoice.changePaymentSettings(PaymentMethod.GATEWAY_BALANCE, null);
+        
+        invoiceRepository.save(invoice);
+
+        Payment payment = Payment.builder().gatewayCode("123")
+                .invoiceId(invoice.getId())
+                .method(invoice.getPaymentSettings().getPaymentMethod())
+                .status(PaymentStatus.PAID)
+                .build();
+
+        Mockito.when(paymentGatewayService.caputre(Mockito.any(PaymentRequest.class))).thenReturn(payment);
+        
+        applicationService.processPayment(invoice.getId());
+        
+        Invoice paidInvoice = invoiceRepository.findById(invoice.getId()).orElseThrow();
+        
+        Assertions.assertThat(paidInvoice.getStatus()).isEqualTo(InvoiceStatus.PAID);
+        
+        Mockito.verify(paymentGatewayService).caputre(Mockito.any(PaymentRequest.class));
+        Mockito.verify(invoicingService).assignPayment(Mockito.any(Invoice.class), Mockito.any(Payment.class));
     }
 }
